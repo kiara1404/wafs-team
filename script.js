582d25b21dd5c65acb18aaed440fd9ec55a75419
ROEL2407 <roel24komen@gmail.com>
2022-02-08 13:19:59 +0100

    Base structure of application

---
+++ script.js
@@ -0,0 +1,12 @@
+// getAndRenderData()
+// function getAndRenderData() {
+//     const getURL = 'https://tribe.api.fdnd.nl/v1/list'
+//     fetch(getURL).then(response => response.json())
+//     .then(response => {
+//         console.log(response)
+//         response.data.forEach(member => {
+//             document.body.insertAdjacentHTML('beforebegin',`<p>${member.memberName}</p>`)
+//         })
+//     }).catch(error => document.body.insertAdjacentHTML('beforebegin', error))
+// }
+
