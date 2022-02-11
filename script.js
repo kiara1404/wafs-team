84a793f701ceef411a6ec20b3e77dd89795ec0c1
ROEL2407 <roel24komen@gmail.com>
2022-02-11 11:33:59 +0100

    Update: added fetch to api

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-// getAndRenderData()
-// function getAndRenderData() {
-//     const getURL = 'https://tribe.api.fdnd.nl/v1/list'
-//     fetch(getURL).then(response => response.json())
-//     .then(response => {
-//         console.log(response)
-//         response.data.forEach(member => {
-//             document.body.insertAdjacentHTML('beforebegin',`<p>${member.memberName}</p>`)
-//         })
-//     }).catch(error => document.body.insertAdjacentHTML('beforebegin', error))
-// }
+getAndRenderData()
 
+memberArray = [];
+
+function getAndRenderData() {
+    const getURL = 'https://tribe.api.fdnd.nl/v1/list'
+    fetch(getURL).then(response => response.json())
+        .then(response => {
+            console.log(response)
+            response.data.forEach(member => {
+                if (member.memberName === "Roel" || member.memberName === "Jorn" || member.memberName === "Kiara" || member.memberName === "Fabian" ) {
+                    memberArray.push({
+                        naam: member.memberName
+                    });
+                }
+            })
+        }).catch(error => document.body.insertAdjacentHTML('beforebegin', error))
+}
+
+console.log(memberArray);
